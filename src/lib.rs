63894485a94246da1db4aa0bb65997064116e0e1
boxed87 <boxed87@outlook.com>
2025-10-27 16:27:05 +0800

    docs(core): adjust documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! ## 核心概念
 //!
 //! - **Actor**: 只需实现 [Actor] trait 即可。
-//! - **Start**: 所有 Actor 自动实现 [ActorExt]，用于快速启动。
+//! - **Start**: 所有 Actor 自动实现 [ActorExt]，可用于快速启动。
 //! - **Inbox / Outbox**: Actor 通过 [inbox] 接收消息，[outbox] 用于发送消息。
 //! - **生命周期**: 发送方负责制，Actor 会在无法再接收消息时自然结束。
 //!
